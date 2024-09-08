cb1612f69a367d8d5a329acd9230ec912b91288a
Shyam Sundar Velmurugan <145095417+HwuShyam2005@users.noreply.github.com>
2024-09-08 17:32:38 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header sticky & back top btn active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 150) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+let lastScrolledPos = 0;
+
+const headerSticky = function () {
+  if (lastScrolledPos >= window.scrollY) {
+    header.classList.remove("header-hide");
+  } else {
+    header.classList.add("header-hide");
+  }
+
+  lastScrolledPos = window.scrollY;
+}
+
+addEventOnElem(window, "scroll", headerSticky);
+
+
+
+/**
+ * scroll reveal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sections[i].classList.add("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
+
+
+
+
+function countdown(endDateString) {
+  let days, hours, minutes, seconds;
+  
+  setInterval(calculate, 1000);
+  
+  function calculate() {
+    let endTimestamp = new Date(
+      Date.parse(endDateString)
+    )
+    let endDate = endTimestamp.getTime()
+
+    let startDate = new Date();
+    startDate = startDate.getTime();
+    
+    let timeRemaining = parseInt((endDate - startDate) / 1000);
+    
+    if (timeRemaining >= 0) {
+      days = parseInt(timeRemaining / 86400);
+      timeRemaining %= 86400;
+      
+      hours = parseInt(timeRemaining / 3600);
+      timeRemaining %= 3600;
+      
+      minutes = parseInt(timeRemaining / 60);
+      timeRemaining %= 60;
+      
+      seconds = parseInt(timeRemaining);
+      
+      document.getElementById("days").innerHTML = parseInt(days, 10);
+      document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+      document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+      document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+    } else {
+      return;
+    }
+  }
+}
+
+// Set the date we're counting down to
+
+let dateString = "September 11, 2024 16:14:00";
+
+countdown(dateString);
